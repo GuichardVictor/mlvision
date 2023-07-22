b7c0a8a5b25c1ec24de5f490d937cdaeb171efda
Guichard Victor <guichardvictor@gmail.com>
2023-07-22 14:49:40 +0200

    add mobileone model for an example of mobile model

--- mlvision/models/__init__.py
+++ mlvision/models/__init__.py
@@ -1,10 +1,12 @@
 from .cnn import SimpleCnn
+from .mobileone import mobileone
 import torchvision.models
 
 MODEL_REGISTRY = {
     "simple_cnn": SimpleCnn,
     "resnet50": torchvision.models.resnet50,
     "resnet18": torchvision.models.resnet18,
+    "mobileone": mobileone
 }
 
 def build_model(architecture, kargs):
